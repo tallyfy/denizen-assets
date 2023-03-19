8abba4ac42dd8e951eb70d4bf423cc792e74e5a7
Muhamed Adham <60203801+MuAdham@users.noreply.github.com>
2023-03-19 19:57:35 +0200

    fix for pixelated variants

--- resize_assets.py
+++ resize_assets.py
@@ -6,15 +6,21 @@ folder = "assets"
 for i in os.listdir(folder):
     if not i.endswith(".md"):
         file = folder + "/" + i
-        im = Image.open(file)
+        
         ## Create small image
-        im = im.resize((640, 480))
-        im.save("assets-small/" + i, "JPEG")
+        im = Image.open(file)
+        im.thumbnail((640, 480))
+        im.save("assets-small/" + i, "JPEG", quality=100)
+        im.close()
         ## Create medium image
-        im = im.resize((1920, 1280))
-        im.save("assets-medium/" + i, "JPEG")
+        im = Image.open(file)
+        im.thumbnail((1920, 1280))
+        im.save("assets-medium/" + i, "JPEG", quality=100)
+        im.close()
         ## Create large image
-        im = im.resize((2400, 1600))
-        im.save("assets-large/" + i, "JPEG")
+        im = Image.open(file)
+        im.resize((2400, 1600))
+        im.save("assets-large/" + i, "JPEG", quality=100)
+        im.close()
 
 os.system("cd assets-small && git add . && cd .. && cd assets-medium && git add . && cd .. && cd assets-large && git add .")
