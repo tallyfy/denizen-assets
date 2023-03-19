33a4bfd917f52dba51f68c23c0d66f6a1f3aea43
Muhamed Adham <60203801+MuAdham@users.noreply.github.com>
2023-03-19 20:20:55 +0200

    removed `quality`

--- resize_assets.py
+++ resize_assets.py
@@ -10,17 +10,17 @@ for i in os.listdir(folder):
         ## Create small image
         im = Image.open(file)
         im.thumbnail((640, 480))
-        im.save("assets-small/" + i, "JPEG", quality=100)
+        im.save("assets-small/" + i, "JPEG")
         im.close()
         ## Create medium image
         im = Image.open(file)
         im.thumbnail((1920, 1280))
-        im.save("assets-medium/" + i, "JPEG", quality=100)
+        im.save("assets-medium/" + i, "JPEG")
         im.close()
         ## Create large image
         im = Image.open(file)
         im.resize((2400, 1600))
-        im.save("assets-large/" + i, "JPEG", quality=100)
+        im.save("assets-large/" + i, "JPEG")
         im.close()
 
 os.system("cd assets-small && git add . && cd .. && cd assets-medium && git add . && cd .. && cd assets-large && git add .")
